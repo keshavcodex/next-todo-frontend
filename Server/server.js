654ab64fd0b474fc387ed1f2ecdac500bc7a103e
GulamMdKhalid <80089845+GulamMd@users.noreply.github.com>
2022-10-30 15:21:23 +0530

    Backend Server Added

---
+++ Server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const body_parse = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { database_uri, port } = require('./config/config');
+// require('dotenv').config();
+
+const app = express();
+const server_port = port || 5000;
+
+app.use(cors());
+app.use(body_parse.json());
+
+const uri = database_uri;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+const tasksRouter = require('./routes/tasks');
+
+//Basic route path
+app.use('/tasks', tasksRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${server_port}`);
+});
